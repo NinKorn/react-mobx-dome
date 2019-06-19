5e066c92b12f9b64d2970fbfdf7317a16e3b7c2f
NinKorn <NinKorn@163.com>
2019-06-19 13:46:03 +0800

    安装router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import routes from './router/router';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+            <div>
+                <Switch>
+                    {/* //主要逻辑在这里 */}
+                    {
+                        routes.map((item, i) => {
+                            if(item.exact){
+                                return <Route exact path={item.path} component={item.component}  key={i}/>
+                            }else{
+                                return <Route path={item.path} component={item.component}  key={i}/>
+                            }
+                        }) 
+                    }
+                </Switch> 
+            </div>
+        </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import '../src/styles/public.css';
-// import App from './App';
-import Home from './pages/Home';
+import App from './App';
+// import Home from './pages/Home';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
